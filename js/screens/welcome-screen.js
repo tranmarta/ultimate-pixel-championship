b46fdf136cf7f2132d89b497dd058861002741bc
Marta <tran.marta@gmail.com>
2023-08-22 14:27:09 +0200

    feat/welcomescreen

--- js/screens/welcome-screen.js
+++ js/screens/welcome-screen.js
@@ -3,3 +3,51 @@ export const welcomeScreen = () => {
    welcomeStepWrapper.innerText = 'Welcome screen';
    return welcomeStepWrapper;
 };
+
+function orangeButton() {
+   const chooseButton = document.createElement('button');
+   const contentButton = document.innerText('Choose');
+   const srcButton = document.src('./components/choose.svg');
+   document.chooseButton.appendChild(contentButton && srcButton);
+   return chooseButton;
+}
+
+/*
+button.addEventListener('click', function (event) {});
+*/
+
+image.onClick = function Choose() {
+   image.src = './components/choose.svg';
+   return image.src;
+}
+
+function UltimatePixelChampionships(fontWelcomeScreen, fontForm) {
+   const fontWelcomeScreen = document.createElement('div');
+   fontWelcomeScreen.innerText = src('./assets/Ultimate Pixel Championships.svg');
+   const fontForm = document.createElement('div');
+   fontForm.innerText = src('./assets/fill-Out-Form.svg')
+   return fontWelcomeScreen && fontForm;
+}
+
+function lineBelowUPC(orangeLine) {
+   const orangeLine = document.createElement('div');
+   orangeLine = src('./components/Line 15.svg');
+   return orangeLine;
+}
+
+function margins(marginNav,marginFooter) {
+   const marginNav = document.createElement('div');
+   marginNav = src('./components/Margin-nav.svg');
+   const marginFooter = document.createElement('div');
+   marginFooter = src('./components/Margin-footer.svg');
+   return marginNav && marginFooter;
+}
+
+function swords(leftSword, rightSword)
+{
+   const leftSword = document.createElement('div');
+   leftSword = src('./components/leftSword.png');
+   const rightSword = document.createElement('div');
+   rightSword = src('./component/rightSword.png');
+   return leftSword && rightSword;
+}
